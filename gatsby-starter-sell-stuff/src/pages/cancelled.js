18a03f4dc1e8f5f5e7244acea397bca697afb738
HAINES-DESKTOP\richa <hello@richardhaines.dev>
2021-01-11 20:24:08 +0100

    filled in blank components

---
+++ gatsby-starter-sell-stuff/src/pages/cancelled.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Cancelled = () => {
+
+    return (
+        <div>
+            <h1>oh no!</h1>
+        </div>
+    )
+}
+
+export default Cancelled;
---
+++ gatsby-starter-sell-stuff/src/pages/success.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Success = () => {
+
+    return (
+        <div>
+            <h1>Yay!</h1>
+        </div>
+    )
+}
+
+export default Success;

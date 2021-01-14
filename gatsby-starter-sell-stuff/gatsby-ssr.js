f3d75cded1cb9f7b0f2709fa9921aa080be897ae
HAINES-DESKTOP\richa <hello@richardhaines.dev>
2021-01-14 13:59:23 +0100

    duplicating instead of wrap-root file

--- gatsby-starter-sell-stuff/gatsby-ssr.js
+++ gatsby-starter-sell-stuff/gatsby-ssr.js
@@ -1,4 +1,53 @@
-import { wrapRootElement as wrap } from './root-wrap';
+// import { wrapRootElement as wrap } from './root-wrap';
+// export const wrapRootElement = wrap
 
+import React from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { CartProvider } from 'use-shopping-cart';
+import { useSiteMetadata } from './src/components/useSiteMetadata';
+import SiteProvider from './src/components/context';
+import {
+    ApolloProvider,
+    ApolloClient,
+    createHttpLink,
+    InMemoryCache,
+} from '@apollo/client';
+import fetch from 'isomorphic-fetch';
 
-export const wrapRootElement = wrap
+const httpLink = createHttpLink({
+    uri: `https://${process.env.SANITY_PROJECT_ID}.api.sanity.io/v1/graphql/${process.env.SANITY_DATASET}/default`,
+    fetch,
+  });
+
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: httpLink,
+    fetch,
+  });
+
+const stripePromise = loadStripe(process.env.STRIPE_API_PUBLIC);
+
+const WithMetaData = ({element}) => {
+    const {currency, allowedCountries} = useSiteMetadata();
+    return(
+        <CartProvider
+        stripe={stripePromise}
+        successUrl="https://gatsby-starter-sell-stuff.netlify.app/success"
+        cancelUrl="https://gatsby-starter-sell-stuff.netlify.app/cancelled"
+        currency={currency}
+        allowedCountries={allowedCountries}
+        billingAddressCollection={true}
+        mode="checkout-session"
+        >
+            <ApolloProvider client={client}>
+                <SiteProvider>
+                    {element}
+                </SiteProvider>
+            </ApolloProvider>
+        </CartProvider>
+    )
+}
+
+export const wrapRootElement = ({element}) => {
+    return <WithMetaData element={element}/>
+}
